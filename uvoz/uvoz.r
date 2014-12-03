1250950137f5cb9dc8279744698e2f174479c167
Damjan Manevski <dame_mane@hotmail.com>
2014-12-03 10:00:32 +0100

    delujoč program

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -28,6 +28,32 @@ uvozitabela1 <- function() {
                     fileEncoding = "UTF-8"))
 }
 
-# Zapišimo podatke v razpredelnico tabela1.ll
+# Zapišimo podatke v razpredelnico tabela1.
 cat("Uvažam podatke...\n")
 t1 <- uvozitabela1()
+
+#Tabela 2
+
+uvozitabela2 <- function() {
+  return(read.csv("podatki/tabela2.csv", sep = ",", as.is = TRUE,
+                    col.names = c("Date", "Municipality", "Trip Duration","Membership Type","No. of Trips",
+                                  "Miles Travelled"),
+                    fileEncoding = "UTF-8"))
+}
+
+# Zapišimo podatke v razpredelnico tabela2.
+cat("Uvažam podatke...\n")
+t2 <- uvozitabela2()
+
+#Tabela 3
+
+uvozitabela3 <- function() {
+  return(read.csv("podatki/tabela3.csv", sep = ",", as.is = TRUE,
+                    col.names = c("Year", "Month", "Station Name","Municipality","Departures","Arrivals"),
+                    fileEncoding = "UTF-8"))
+}
+
+# Zapišimo podatke v razpredelnico tabela3.
+cat("Uvažam podatke...\n")
+t3 <- uvozitabela3()
+
