c642a8c0293d5e70c49eac020b4c1ca472a48134
Damjan Manevski <dame_mane@hotmail.com>
2014-12-15 12:06:41 +0100

    slike

--- slike/grafi.R
+++ slike/grafi.R
@@ -1,6 +1,6 @@
 # Prvi graf - izračun
 # Seštejemo kolikokrat uporabijo kolesa vo različnih dnev. 0-Nedelja, 1-Ponedeljek, 2-Torek...
-k2 <- sapply(0:6, function(x) sum(t1[t1[,6] == x, 15]))
+k2 <- sapply(0:6, function(x) sum(t1[t1[,6] == x, 15]))/1000
 names(k2)<-c("Nedelja","Ponedeljek","Torek","Sreda","Četrtek","Petek","Sobota")
 
 #Drugi graf - izračun
@@ -11,7 +11,7 @@ k1< sapply(c("Arlington","Alexandria","Wash.","RMC","Rv./SG"), function(x) sum(t
 
 #Risanje grafov
 pdf("slike/grafi1.pdf",paper="a4")
-pie(k2,main="Uporaba koles v različnih dnev 2011-2012")
+barplot(k2,main="Uporaba koles v različnih dnev 2011-2012")
 barplot(k1,xlab="Različne občine",ylab="Število odhodov in prihodov",
         main="Število izposojenih koles v Juliju 2014",col="red")
 dev.off()
